35c567f42d4851ca0d5a02c84a5c5d9905161b8b
Tanguy Herbron <tanguy.herbron@outlook.com>
2021-04-26 17:26:07 +0200

    Remove bottom bar from polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,11 +11,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # List monitors to load the bars on every one of them
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar top -c ~/.config/polybar/config-top.ini &
-        MONITOR=$m polybar bottom -c ~/.config/polybar/config-bottom.ini &
+        MONITOR=$m polybar -q top -c ~/.config/polybar/config-top.ini &
+        # MONITOR=$m polybar -q bottom -c ~/.config/polybar/config-bottom.ini &
     done
 else
     # Launch bar1 and bar2
-    polybar top -c ~/.config/polybar/config-top.ini &
-    polybar bottom -c ~/.config/polybar/config-bottom.ini &
+    polybar -q top -c ~/.config/polybar/config-top.ini &
+    # polybar -q bottom -c ~/.config/polybar/config-bottom.ini &
 fi

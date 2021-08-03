ed353147431364c5d7b0cb58d65c6dc1895e0bbe
Tanguy Herbron <tanguy.herbron@outlook.com>
2021-08-03 09:14:37 +0200

    Fix multihead file detection issue

--- multihead.sh
+++ multihead.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ /tmp/multihead ] ; then
+if [ -f "/tmp/multihead" ] ; then
     rm /tmp/multihead
     # xrandr --auto --output HDMI-0 --right-of eDP-1-1 && xrandr --auto --output DP-1-1 --right-of HDMI-0
     xrandr --auto --output DP-1-1 --left-of eDP-1-1 && xrandr --auto --output HDMI-0 --left-of DP-1-1
